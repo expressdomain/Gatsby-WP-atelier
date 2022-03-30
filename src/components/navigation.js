48d9acf440c425d291e085982f6c0c774ad3286a
expressdomain <102648513+expressdomain@users.noreply.github.com>
2022-03-30 15:49:48 +0530

    Update navigation.js
    menu query update

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,27 +1,36 @@
 import React from 'react'
 import { Link, useStaticQuery, graphql } from "gatsby"
 
-const Nav = (props) => {
-    const data = useStaticQuery(graphql`
-    query NavQuery {
-        wpMenu(name: {eq: "main"}) {
-          menuItems {
-            nodes {
-              id
-              label
-              url
-            }
+const Nav = () => {
+  const nav = useStaticQuery(graphql`
+    {
+      wpMenu(locations: {eq: GATSBY_HEADER_MENU}) {
+        menuItems {
+          nodes {
+            label
+            url
+            target
           }
         }
-      }      
+      }
+    }
   `)
-  const menu = data.wpMenu.menuItems.nodes
 
-    return (
-        <nav>
-            {menu.map((link) => <Link key={link.id} to={link.url}>{link.label}</Link> )}
-        </nav>
-    )
+  return (
+    <ul className="menu">
+      {nav.wpMenu.menuItems.nodes.map((menuItem,i) => (
+        <li className="menu__item" key={'menuItem_' + i}>
+          <Link 
+            to={menuItem.url} 
+            className="menu__item-link" 
+            activeClassName="menu__item-link--active"
+            partiallyActive={true}>
+            {menuItem.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default Nav;
